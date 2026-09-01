74f5f973b8d09e0d29e3234113868ef873053248
Seongho Bae <me@seonghobae.me>
2026-09-02 03:01:20 +0900

    test: bind Pingora keepalive pool to edge contract

--- tests/runtime_policy.rs
+++ tests/runtime_policy.rs
@@ -4,11 +4,12 @@ use cwl_pingora_gateway::runtime_policy::{
 };
 
 #[test]
-fn version_one_overrides_pingora_retry_and_shutdown_defaults() {
-    let conf = build_server_conf();
+fn version_one_overrides_pingora_retry_capacity_and_shutdown_defaults() {
+    let conf = build_server_conf(32);
 
     assert_eq!(V1_MAX_UPSTREAM_ATTEMPTS, 1);
     assert_eq!(conf.max_retries, V1_MAX_UPSTREAM_ATTEMPTS);
+    assert_eq!(conf.upstream_keepalive_pool_size, 32);
     assert_eq!(conf.grace_period_seconds, Some(V1_GRACE_PERIOD_SECONDS));
     assert_eq!(
         conf.graceful_shutdown_timeout_seconds,
