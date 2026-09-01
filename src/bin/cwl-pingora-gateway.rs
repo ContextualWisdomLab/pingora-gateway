cbe76d1c8c24e631125f4a4e019f5216a1033bc3
Seongho Bae <me@seonghobae.me>
2026-09-01 14:54:40 +0900

    feat(observability): expose explicit Prometheus listener

--- src/bin/cwl-pingora-gateway.rs
+++ src/bin/cwl-pingora-gateway.rs
@@ -13,6 +13,8 @@ use cwl_pingora_gateway::startup::GatewayCommand;
 use pingora::prelude::{http_proxy_service, Server};
 
 fn main() {
+    env_logger::init();
+
     let command =
         GatewayCommand::parse(env::args_os()).unwrap_or_else(|error| exit_with_error(error));
     let config = command
@@ -21,6 +23,7 @@ fn main() {
     let proxy =
         GatewayProxy::try_from_config(&config).unwrap_or_else(|error| exit_with_error(error));
     let listener = config.listener.to_string();
+    let metrics_listener = config.metrics_listener.to_string();
 
     let mut server = Server::new(None).unwrap_or_else(|error| exit_with_error(error));
     server.bootstrap();
@@ -28,6 +31,11 @@ fn main() {
     let mut proxy_service = http_proxy_service(&server.configuration, proxy);
     proxy_service.add_tcp(&listener);
     server.add_service(proxy_service);
+
+    let mut metrics_service = pingora_prometheus::prometheus_http_service();
+    metrics_service.add_tcp(&metrics_listener);
+    server.add_service(metrics_service);
+
     server.run_forever();
 }
 
