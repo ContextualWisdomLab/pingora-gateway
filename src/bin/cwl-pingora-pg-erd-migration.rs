b9a955aa3b74e915d869223dd29277ad44c64951
Seongho Bae <me@seonghobae.me>
2026-09-03 04:27:33 +0900

    fix: enforce payload-safe logging in pg-erd runtime

--- src/bin/cwl-pingora-pg-erd-migration.rs
+++ src/bin/cwl-pingora-pg-erd-migration.rs
@@ -8,6 +8,7 @@ use std::env;
 use std::fs;
 use std::process::ExitCode;
 
+use cwl_pingora_gateway::logging_policy::init_runtime_logging;
 use cwl_pingora_gateway::migration_admin::PgErdMigrationConfig;
 use cwl_pingora_gateway::runtime_policy::build_server_conf;
 use cwl_pingora_gateway::startup::GatewayCommand;
@@ -15,7 +16,11 @@ use pingora::prelude::{http_proxy_service, Server};
 use pingora::server::RunArgs;
 
 fn main() -> ExitCode {
-    env_logger::init();
+    if let Err(error) = init_runtime_logging() {
+        return exit_with_error(&format!(
+            "unable to initialize payload-safe runtime logging: {error}"
+        ));
+    }
 
     let args: Vec<_> = env::args_os().collect();
     let command = match GatewayCommand::parse(&args) {
