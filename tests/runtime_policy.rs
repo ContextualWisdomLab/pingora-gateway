570861766a146702eeb507eafbade206b2915006
Seongho Bae <me@seonghobae.me>
2026-09-10 18:38:30 +0900

    test: pin compatibility service worker topology

--- tests/runtime_policy.rs
+++ tests/runtime_policy.rs
@@ -1,6 +1,6 @@
 use cwl_pingora_gateway::runtime_policy::{
-    build_server_conf, V1_GRACEFUL_SHUTDOWN_TIMEOUT_SECONDS, V1_GRACE_PERIOD_SECONDS,
-    V1_MAX_UPSTREAM_ATTEMPTS,
+    build_server_conf, V1_DEFAULT_SERVICE_THREADS, V1_GRACEFUL_SHUTDOWN_TIMEOUT_SECONDS,
+    V1_GRACE_PERIOD_SECONDS, V1_MAX_UPSTREAM_ATTEMPTS,
 };
 
 #[test]
@@ -9,6 +9,7 @@ fn version_one_overrides_pingora_retry_capacity_and_shutdown_defaults() {
 
     assert_eq!(V1_MAX_UPSTREAM_ATTEMPTS, 1);
     assert_eq!(conf.max_retries, V1_MAX_UPSTREAM_ATTEMPTS);
+    assert_eq!(conf.threads, V1_DEFAULT_SERVICE_THREADS);
     assert_eq!(conf.upstream_keepalive_pool_size, 32);
     assert_eq!(conf.grace_period_seconds, Some(V1_GRACE_PERIOD_SECONDS));
     assert_eq!(
